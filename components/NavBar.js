23a943ada80b90ac707a9835d7d8394116c8baa7
Vashishth Gajjar <gajjarv2001@gmail.com>
2024-04-10 19:55:31 -0500

    header and logo made

---
+++ components/NavBar.js
@@ -0,0 +1,30 @@
+'use client'; // enables client-side rendering
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import {useState, useEffect} from 'react'
+import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
+
+const NavBar = () => {
+  return ( 
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href='/' className='flex gap-2 flex-center'>
+        <Image src="/assets/images/logo.svg"
+        alt='logo'
+        width={30}
+        height={30}
+        className='object-contain'/>
+
+        <p className='logo_text'>Prompt Genuis</p>
+      </Link>
+
+      {/* mobile  */}
+
+      <div className='sm-flex hidden'>
+
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
